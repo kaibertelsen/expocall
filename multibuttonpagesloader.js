21d16d2c6d60d8c6c2a76b25208e6e1a44d818e4
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2024-11-25 13:03:30 +0100

    Update multibuttonpagesloader.js

--- multibuttonpagesloader.js
+++ multibuttonpagesloader.js
@@ -1,1 +1,27 @@
+function loadScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => resolve();
+        script.onerror = () => reject(`Failed to load script: ${url}`);
+        document.head.appendChild(script);
+    });
+}
+
+// Liste over CDN-URL-er som skal lastes inn
+const cdnScripts = [
+    "https://kaibertelsen.github.io/innkjopsgruppen/value/followup-functions.js",
+    "https://kaibertelsen.github.io/innkjopsgruppen/value/followup-listfunction.js"
+
+    
+];
+
+// Laste inn alle skriptene sekvensielt
+cdnScripts.reduce((promise, script) => {
+    return promise.then(() => loadScript(script));
+}, Promise.resolve()).then(() => {
+    console.log("All scripts loaded");
+}).catch(error => {
+    console.error(error);
+});
 
