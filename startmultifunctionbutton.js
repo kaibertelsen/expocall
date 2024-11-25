1657f64289103fe5bc3c96f916225208e9ef977e
Kai Bertelsen <kai@exposoft.no>
2024-11-25 15:50:29 +0100

    ny

--- startmultifunctionbutton.js
+++ startmultifunctionbutton.js
@@ -1,4 +1,6 @@
 
+document.getElementById("multibuttonstart").addEventListener("click", getPremessage);
+
 function getPremessage() {
     var body = airtablebodylistAND({klientid:klientid,standard:1});
     Getlistairtable("appYyqoMRDdL08VXJ","tbljbPtkSRhx2U9IG",body,"respondPremessage");
